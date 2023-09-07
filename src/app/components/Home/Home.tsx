9db0b2347e9393b646aeeafac19b6511c7943815
fatmakhv <fatmaba@gmail.com>
2023-09-07 13:34:02 +0300

    Reorg types

--- src/app/components/Home/Home.tsx
+++ src/app/components/Home/Home.tsx
@@ -1,11 +1,11 @@
 'use client';
 
-import { HomeProps } from '@/shared/types/Types';
-import React, { FC } from 'react';
+import { LogoutProps } from '@/shared/types/Types';
+import { FC } from 'react';
 import Card from '../UI/Card/Card';
 import './Home.css';
 
-const Home: FC<HomeProps> = ({ onLogout }) => {
+const Home: FC<LogoutProps> = ({ onLogout }) => {
   return (
     <Card cssName="home">
       <h1>Welcome back!</h1>
